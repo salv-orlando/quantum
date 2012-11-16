132b50f7a2e21c6c3d3f589a5a29840143b0f27d
Gary Kotton <gkotton@redhat.com>
2012-11-16 00:20:33 +0000

    Prevent router being deleted if it is used by a floating IP
    Fixes bug 1080638
    
    Change-Id: I10768044aac3fe0ce994e42e798df754478d98d1

--- quantum/tests/unit/test_l3_plugin.py
+++ quantum/tests/unit/test_l3_plugin.py
@@ -971,6 +971,9 @@ class L3NatDBTestCase(test_db_plugin.QuantumDbPluginV2TestCase):
                     port_id=p['port']['id'])
                 self.assertEqual(res.status_int, exc.HTTPCreated.code)
                 floatingip = self.deserialize(fmt, res)
+                self._remove_external_gateway_from_router(
+                    r['router']['id'],
+                    public_sub['subnet']['network_id'])
                 self._delete('routers', r['router']['id'],
                              expected_code=exc.HTTPConflict.code)
                 # Cleanup
