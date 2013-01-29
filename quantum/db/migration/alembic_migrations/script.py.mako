openstack/neutron

Add migration support to Quantum
implements blueprint quantum-db-upgrades

This changeset provide database migration capabilities to Quantum by
wrapping the Alembic library.

Change-Id: I8ba3a07f5a65e0fda9c0e85ed9c07c5978c53bc7

quantum/db/migration/alembic_migrations/script.py.mako
@@ -1,0 +1,54 @@
+# vim: tabstop=4 shiftwidth=4 softtabstop=4
+#
+# Copyright ${create_date.year} OpenStack LLC
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+#
+
+"""${message}
+
+Revision ID: ${up_revision}
+Revises: ${down_revision}
+Create Date: ${create_date}
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = ${repr(up_revision)}
+down_revision = ${repr(down_revision)}
+
+# Change to ['*'] if this migration applies to all plugins
+
+migration_for_plugins = [
+    '${config.quantum_config.core_plugin}'
+]
+
+from alembic import op
+import sqlalchemy as sa
+${imports if imports else ""}
+
+from quantum.db import migration
+
+
+def upgrade(active_plugin=None, enable_db_quota=False):
+    if not migration.should_run(active_plugin, migration_for_plugins):
+        return
+
+    ${upgrades if upgrades else "pass"}
+
+
+def downgrade(active_plugin=None, enable_db_quota=False):
+    if not migration.should_run(active_plugin, migration_for_plugins):
+        return
+
+    ${downgrades if downgrades else "pass"}

salv-orlando/quantum

This patch backports all the commits related to this blueprint.
Issue: #9528

quantum/db/migration/alembic_migrations/script.py.mako
@@ -1,0 +1,54 @@
+# vim: tabstop=4 shiftwidth=4 softtabstop=4
+#
+# Copyright ${create_date.year} OpenStack LLC
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+#
+
+"""${message}
+
+Revision ID: ${up_revision}
+Revises: ${down_revision}
+Create Date: ${create_date}
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = ${repr(up_revision)}
+down_revision = ${repr(down_revision)}
+
+# Change to ['*'] if this migration applies to all plugins
+
+migration_for_plugins = [
+    '${config.quantum_config.core_plugin}'
+]
+
+from alembic import op
+import sqlalchemy as sa
+${imports if imports else ""}
+
+from quantum.db import migration
+
+
+def upgrade(active_plugin=None, options=None):
+    if not migration.should_run(active_plugin, migration_for_plugins):
+        return
+
+    ${upgrades if upgrades else "pass"}
+
+
+def downgrade(active_plugin=None, options=None):
+    if not migration.should_run(active_plugin, migration_for_plugins):
+        return
+
+    ${downgrades if downgrades else "pass"}