a201d7cabaede25549549a8ee25502d753a38945
Aaron Rosen <arosen@nicira.com>
2013-05-14 19:14:44 -0700

    Revert "parallelize initial sync state for dhcp agent"
    This reverts commit d23814dea3b08fae581793b94669ac59828caa16.
    
    Change-Id: I2214c7dabf0f2ec961ce10ebbbcdc513bc73664d

--- quantum/common/utils.py
+++ quantum/common/utils.py
@@ -25,8 +25,6 @@ import logging as std_logging
 import os
 import signal
 import socket
-from threading import Thread
-from Queue import Queue
 
 from eventlet.green import subprocess
 from oslo.config import cfg
@@ -195,38 +193,3 @@ def is_extension_supported(plugin, ext_alias):
 
 def log_opt_values(log):
     cfg.CONF.log_opt_values(log, std_logging.DEBUG)
-
-
-class Worker(Thread):
-    """Thread executing tasks from a given tasks queue"""
-    def __init__(self, tasks):
-        Thread.__init__(self)
-        self.tasks = tasks
-        self.daemon = True
-        self.start()
-
-    def run(self):
-        while True:
-            func, args, kargs = self.tasks.get()
-            try:
-                func(*args, **kargs)
-            except Exception, e:
-                print e
-            finally:
-                self.tasks.task_done()
-
-
-class ThreadPool:
-    """Pool of threads consuming tasks from a queue"""
-    def __init__(self, num_threads):
-        self.tasks = Queue(num_threads)
-        for _ in range(num_threads):
-            Worker(self.tasks)
-
-    def add_task(self, func, *args, **kargs):
-        """Add a task to the queue"""
-        self.tasks.put((func, args, kargs))
-
-    def wait_completion(self):
-        """Wait for completion of all the tasks in the queue"""
-        self.tasks.join()
